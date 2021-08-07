7d6718d02ac2b209daacd3942aea409c3216dc02
Charvi Jindal <charvijindal@gmail.com>
2021-08-07 22:23:03 +0530

    Added pre parse

--- Simple-Assembler/assembler.py
+++ Simple-Assembler/assembler.py
@@ -15,10 +15,74 @@ type_E = {'jmp':'01111', 'jlt': '10000', 'jgt': '10001', 'je': '10010'}
 
 type_F = {'hlt': '10011'}
 
+#{var_name : [address(binary), value]}
+var_dic = {}
+
+#{label_name : address(binary)}
+labels = {}
+
+#In order: code -> hlt
+#{address(decimal) : entire instruction(string)}
+#for labels, the label name is not included
+line_dic = {}
+
+
 def pre_parse():
-	var = {}
-	labels = {}
-	lines = {}
+	var_temp = []
+	
+	address = 0
+	for line in f:
+		if line == "\n":
+			continue
+		words = line.split()
+
+		if words[0] == "var":
+
+			if words[1] in var_temp:
+				print("Syntax Error: 2 or more variables cannot have the same name.")
+				quit()
+
+			elif words[1] in labels.keys():
+				print("Syntax Error: Variables and labels can't have the same name.")
+				quit()
+
+			else:
+				var_temp.append(words[1])
+
+		elif words[0][-1] == ":":
+			
+			if words[0][:-1] in var_temp:
+				print("Syntax Error: Variables and labels can't have the same name.")
+				quit()
+
+			elif words[0][:-1] in labels.keys():
+				print("Syntax Error: 2 or more variables cannot have the same name.")
+				quit()
+
+			else:
+				labels[words[0][:-1]] = '{0:08b}'.format(address)
+				if(line[-1] == "\n"):
+					line_dic[address] = line[len(words[0])+1:-1]
+				else:
+					line_dic[address] = line[len(words[0])+1:]
+			address += 1
+
+		else:
+			if(line[-1] == "\n"):
+				line_dic[address] = line[:-1]
+			else:
+				line_dic[address] = line
+			
+			address +=1
+
+	for v in var_temp:
+		var_dic[v] = ['{0:08b}'.format(address),0]
+		address += 1
+
+	if(address>257):
+		print("Error: Number of instructions exceed 256")
+		quit()
+
 
 
 def parse_A(instruction, words):
@@ -56,7 +120,6 @@ def parse_B(instruction, words):
 	code = opcode + bin_r1 + bin_imm
 	print(code)
 
-
 def parse_C(instruction, words):
 	opcode = type_C[instruction]
 
@@ -70,7 +133,7 @@ def parse_C(instruction, words):
 	r2 = int(words[2][1])
 	bin_r2 = '{0:03b}'.format(r2)
 
-	code = opcode + '00000' + bin_r1 + bin_r2;
+	code = opcode + '00000' + bin_r1 + bin_r2
 	print(code)
 
 #check how memory addresses are given in assembly ->  AS LABELS
@@ -78,15 +141,14 @@ def parse_C(instruction, words):
 def parse_D(instruction, words):
 	opcode = type_D[instruction]
 
-	if((words[1][0] == 'R' and words[1][1].isdecimal() and words[2] in labels) == 0):
+	if((words[1][0] == 'R' and words[1][1].isdecimal() and words[2] in var_dic.keys()) == 0):
 		print('Invalid syntax: Type D instruction cannot be interpreted in this way')
 		quit()
 
 	r1 = int(words[1][1])
 	bin_r1 = '{0:03b}'.format(r1)
 
-	mem = int(labels[words[2]])
-	bin_mem = '{0:08b}'.format(imm)
+	bin_mem = var_dic[words[2]][0]
 
 	code = opcode + bin_r1 + bin_mem #check binary memory
 	print(code)
@@ -94,12 +156,11 @@ def parse_D(instruction, words):
 def parse_E(instruction, words):
 	opcode = type_E[instruction]
 
-	if((words[1] in labels) == 0):
+	if((words[1] in labels.keys()) == 0):
 		print('Invalid syntax: Type E instruction cannot be interpreted in this way')
 		quit()
 
-	mem = int(labels[words[1]])
-	bin_mem = '{0:08b}'.format(imm)
+	bin_mem = labels[words[1]]
 
 	code = opcode + '000' + bin_mem #check binary memory
 	print(code)
@@ -109,9 +170,10 @@ def parse_F(instruction, words):
 	code = opcode + '0'*11
 	print(code)
 
+pre_parse()
 
-for line in f:
-	words = line.split()
+for address in line_dic.keys():
+	words = line_dic[address].split()
 
 	if words[0] in type_A:
 		instruction = words[0]
